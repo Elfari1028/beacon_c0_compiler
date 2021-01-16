347e30cc12b5e7bd5e51ff1647615c08bd956ae2
Alphar Abdugeni <40574888+alpharpledez@users.noreply.github.com>
2021-01-16 13:56:04 +0800

    return num main

--- bin/main.dart
+++ bin/main.dart
@@ -1,11 +1,12 @@
 import 'instruction/output.dart';
 
-void main(List<String> args) {
+int main(List<String> args) {
   try {
     var outPut = OutPut(args[0], args[1]);
-
     outPut.start();
+    return 0;
   } catch (e) {
     print(e);
+    return 1;
   }
 }
